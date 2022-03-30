71b5aaa02b79de528705cda05fa69b5ce5960245
swathi.chadalavada <swathi.chadalavada@servicenow.com>
2022-03-30 10:38:20 -0700

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,35 +1,50 @@
 from . import Expense
 
-expenses = Expense.Expenses()
-expenses.read_expenses('data/spending_data.csv')
-
-for expense in expenses.list:
-    myBudgetList.append(expense.amount)
-print("The count of all expenses: " + str(len(myBudgetList)))
 
 class BudgetList:
-    self.budget = budget
-    self.sum_expenses = 0
-    self.expenses = []
-    self.sum_overages = 0
-    self.sum_overages = []
-    if(self.sum_expenses + item  < self.budget):
-       self.expenses.append(item)
+    def __init__(self, budget):
+        self.budget = budget
+        self.sum_expenses = 0
+        self.expenses = []
+        self.sum_overages = 0
+        self.sum_overages = []
+
+    def __iter__(self):
+        self.iter_e = iter(self.expenses)
+        self.iter_o = iter(self.sum_overages)
+        return self
+
+    def __next__(self):
+        try:
+            return self.iter_e.__next__()
+        except StopIteration a stop:
+            return self.iter_o.__next__()
+
+
+def append(self, item):
+    if (self.sum_expenses + item < self.budget):
+        self.expenses.append(item)
+        self.sum_overages += item
+    else:
+        self.overages.append(item)
         self.sum_overages += item
 
-class main():
-    myBudgetList = budgetList(1200)
-if(__name__ == "__main__")
-    main()
 
-    def append(self, item):
-        pass
-
-    def _len_():
-        len(self.expenses) + len(self.overages)
+def _len_(self):
+    return sum(self.expenses), len(self.overages)
 
 
+def main():
+    myBudgetList = BudgetList(1200)
+    expenses = Expense.Expenses()
+    expenses.read_expenses('data/spending_data.csv')
 
+    for expense in expenses.list:
+        myBudgetList.append(expense.amount)
+    print("The count of all expenses: " + str(len(myBudgetList)))
+
+    for entry in myBudgetList:
+        print(entry)
 
 
 

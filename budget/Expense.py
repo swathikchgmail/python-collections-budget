9934ecb081561cf861dd382c4ca8b1683bda419a
swathi.chadalavada <swathi.chadalavada@servicenow.com>
2022-03-31 13:22:46 -0700

    updated expenses and expenses category

--- budget/Expense.py
+++ budget/Expense.py
@@ -40,3 +40,17 @@ class Expenses():
                     unnecessary_expenses.add(i)
             
             return [necessary_expenses, food_expenses, unnecessary_expenses]
+
+    def categorize_set_comprehension(self):
+        necessary_expenses = {
+            x for x in self.list :
+              x.category == 'Phone' ||
+              x.category == 'Auto and Gas' ||
+              x.category == 'Classes' ||
+              x.category == 'Utilities' ||
+              x.category == 'Mortgage'
+        }
+        food_expenses = x.category == 'Groceries' || x.category == 'Eating Out'
+        unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
+        return necessary_expenses, unnecessary_expenses, food_expenses
+
